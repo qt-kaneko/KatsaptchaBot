24fdbc0ca2d9570956c06fdfc8f42bffdc20ce2a
​ <mail.qt.kaneko@gmail.com>
2023-08-16 03:13:58 +0300

    ​

--- Program.cs
+++ Program.cs
@@ -6,119 +6,109 @@ using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
-class Program
+Dictionary<long, Message> _pending = new();
+
+var bot = new TelegramBotClient(Environment.GetEnvironmentVariable("TOKEN")!);
+bot.StartReceiving(OnUpdate, OnPollingError, new ReceiverOptions() {
+  ThrowPendingUpdates = true,
+  AllowedUpdates = new UpdateType[] {
+    UpdateType.ChatMember,
+    UpdateType.CallbackQuery,
+  }
+});
+
+await Task.Delay(Timeout.Infinite);
+
+Task OnUpdate(ITelegramBotClient bot, Update update, CancellationToken ct)
 {
-  TelegramBotClient _bot = new(Environment.GetEnvironmentVariable("TOKEN")!);
-
-  Dictionary<long, Message> _pending = new();
-
-  public Program()
+  switch (update.Type)
   {
-    _bot.StartReceiving(OnUpdate, OnPollingError, new ReceiverOptions() {
-      ThrowPendingUpdates = true,
-      AllowedUpdates = new UpdateType[] {
-        UpdateType.ChatMember,
-        UpdateType.CallbackQuery,
-      }
+    case UpdateType.ChatMember: return OnChatMember(bot, update.ChatMember!, ct);
+    case UpdateType.CallbackQuery: return OnCallbackQuery(bot, update.CallbackQuery!, ct);
+    default: throw new ArgumentOutOfRangeException(nameof(update.Type));
+  }
+}
+async Task OnChatMember(ITelegramBotClient bot, ChatMemberUpdated chatMember, CancellationToken ct)
+{
+  if (chatMember.From.Id == bot.BotId) return;
+
+  var chat = chatMember.Chat;
+
+  if (chatMember.NewChatMember is ChatMemberMember newChatMember)
+  {
+    var user = newChatMember.User;
+
+    if (user.IsBot) return;
+
+    await bot.RestrictChatMemberAsync(chat, user.Id, new ChatPermissions() {
+      CanSendMessages = false,
+      CanSendAudios = false,
+      CanSendDocuments = false,
+      CanSendPhotos = false,
+      CanSendVideos = false,
+      CanSendVideoNotes = false,
+      CanSendVoiceNotes = false
     });
 
-    Thread.Sleep(Timeout.Infinite);
+    var captchaMessage = await bot.SendTextMessageAsync(
+      chat, $"[{user.FirstName}](tg://user?id={user.Id}), чей Крым?",
+      parseMode: ParseMode.Markdown,
+      replyMarkup: new InlineKeyboardMarkup(new InlineKeyboardButton[] {
+        InlineKeyboardButton.WithCallbackData("Украины"),
+        InlineKeyboardButton.WithCallbackData("России"),
+      })
+    );
+    _pending.Add(user.Id, captchaMessage);
   }
-  static void Main() => new Program();
-
-  Task OnUpdate(ITelegramBotClient bot, Update update, CancellationToken ct)
+  else if (chatMember.NewChatMember is ChatMemberLeft chatMemberLeft)
   {
-    switch (update.Type)
-    {
-      case UpdateType.ChatMember: return OnChatMember(bot, update.ChatMember!, ct);
-      case UpdateType.CallbackQuery: return OnCallbackQuery(bot, update.CallbackQuery!, ct);
-      default: throw new ArgumentOutOfRangeException(nameof(update.Type));
-    }
+    var user = chatMemberLeft.User;
+
+    if (!_pending.TryGetValue(user.Id, out var captchaMessage)) return;
+
+    await bot.DeleteMessageAsync(captchaMessage.Chat, captchaMessage.MessageId);
+    _pending.Remove(user.Id);
   }
-  async Task OnChatMember(ITelegramBotClient bot, ChatMemberUpdated chatMember, CancellationToken ct)
+}
+async Task OnCallbackQuery(ITelegramBotClient bot, CallbackQuery callbackQuery, CancellationToken ct)
+{
+  await bot.AnswerCallbackQueryAsync(callbackQuery.Id);
+
+  var user = callbackQuery.From;
+
+  if (!_pending.TryGetValue(user.Id, out var captchaMessage)) return;
+
+  var chat = captchaMessage.Chat;
+
+  await bot.DeleteMessageAsync(captchaMessage.Chat, captchaMessage.MessageId);
+  _pending.Remove(user.Id);
+
+  if (callbackQuery.Data == "Украины")
   {
-    if (chatMember.From.Id == bot.BotId) return;
-
-    var chat = chatMember.Chat;
-
-    if (chatMember.NewChatMember is ChatMemberMember newChatMember)
-    {
-      var user = newChatMember.User;
-
-      if (user.IsBot) return;
-
-      await bot.RestrictChatMemberAsync(chat, user.Id, new ChatPermissions() {
-        CanSendMessages = false,
-        CanSendAudios = false,
-        CanSendDocuments = false,
-        CanSendPhotos = false,
-        CanSendVideos = false,
-        CanSendVideoNotes = false,
-        CanSendVoiceNotes = false
-      });
-
-      var captchaMessage = await bot.SendTextMessageAsync(
-        chat, $"[{user.FirstName}](tg://user?id={user.Id}), чей Крым?",
-        parseMode: ParseMode.Markdown,
-        replyMarkup: new InlineKeyboardMarkup(new InlineKeyboardButton[] {
-          InlineKeyboardButton.WithCallbackData("Украины"),
-          InlineKeyboardButton.WithCallbackData("России"),
-        })
-      );
-      _pending.Add(user.Id, captchaMessage);
-    }
-    else if (chatMember.NewChatMember is ChatMemberLeft chatMemberLeft)
-    {
-      var user = chatMemberLeft.User;
-
-      if (_pending.TryGetValue(user.Id, out var captchaMessage))
-      {
-        await bot.DeleteMessageAsync(captchaMessage.Chat, captchaMessage.MessageId);
-        _pending.Remove(user.Id);
-      }
-    }
+    await bot.RestrictChatMemberAsync(chat, user.Id, new ChatPermissions() {
+      CanSendMessages = true,
+      CanSendAudios = true,
+      CanSendDocuments = true,
+      CanSendPhotos = true,
+      CanSendVideos = true,
+      CanSendVideoNotes = true,
+      CanSendVoiceNotes = true,
+      CanSendPolls = true,
+      CanSendOtherMessages = true,
+      CanAddWebPagePreviews = true,
+      CanChangeInfo = true,
+      CanInviteUsers = true,
+      CanPinMessages = true,
+      CanManageTopics = true
+    });
   }
-  async Task OnCallbackQuery(ITelegramBotClient bot, CallbackQuery callbackQuery, CancellationToken ct)
+  else if (callbackQuery.Data == "России")
   {
-    await bot.AnswerCallbackQueryAsync(callbackQuery.Id);
-
-    var user = callbackQuery.From;
-
-    if (_pending.TryGetValue(user.Id, out var captchaMessage))
-    {
-      var chat = captchaMessage.Chat;
-
-      await bot.DeleteMessageAsync(captchaMessage.Chat, captchaMessage.MessageId);
-      _pending.Remove(user.Id);
-
-      if (callbackQuery.Data == "Украины")
-      {
-        await bot.RestrictChatMemberAsync(chat, user.Id, new ChatPermissions() {
-          CanSendMessages = true,
-          CanSendAudios = true,
-          CanSendDocuments = true,
-          CanSendPhotos = true,
-          CanSendVideos = true,
-          CanSendVideoNotes = true,
-          CanSendVoiceNotes = true,
-          CanSendPolls = true,
-          CanSendOtherMessages = true,
-          CanAddWebPagePreviews = true,
-          CanChangeInfo = true,
-          CanInviteUsers = true,
-          CanPinMessages = true,
-          CanManageTopics = true
-        });
-      }
-      else if (callbackQuery.Data == "России")
-      {
-        await bot.BanChatMemberAsync(chat, user.Id);
-      }
-    }
+    await bot.BanChatMemberAsync(chat, user.Id);
   }
+}
 
-  static async Task OnPollingError(ITelegramBotClient bot, Exception error, CancellationToken ct)
-  {
-    ExceptionDispatchInfo.Throw(error);
-  }
+static async Task OnPollingError(ITelegramBotClient bot, Exception error, CancellationToken ct)
+{
+  ExceptionDispatchInfo.Throw(error);
 }
